c05ed3c91da2594c128d94933147e3b45d2304d9
Student <example@example.com>
2020-03-03 13:17:05 -0800

    make an API call"

---
+++ src/Posts.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Posts extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+   drinks: []
+}
+}
+
+ componentDidMount() {
+   fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+   .then(res => res.json())
+   .then(data => this.setState({  drinks: data.drinks  }));
+ }
+
+ render() {
+   console.log(this.state.drinks)
+   const drinkItems = this.state.drinks &&  this.state.drinks.map(drink => (
+     <div key={drink.idDrink}>
+     <h3>{drink.strDrink} </h3>
+     </div>
+   ));
+   return (
+     <div>
+     <h1> Drinks </h1>
+     {drinkItems}
+     </div>
+
+   )
+ }
+}
+export default Posts
